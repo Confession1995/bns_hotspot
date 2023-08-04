b9eeade23b24a6997093a56f59d0db721e940d0b
QingFeng2336 <331052249@qq.com>
2023-08-04 18:02:07 +0800

    xx

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.get("/", (req, res) => {
         var two=0;
         var three=0;
         var list=res.data;
-        console.log(list.length-1) 
         var minmoney =list[list.length-1].iTicket
         // console.log(res)
         for(var i=0;i<list.length;i++){
@@ -34,7 +33,8 @@ app.get("/", (req, res) => {
             <h1>总金额:`+sumcount+`</h1><br><br>
             <h3>No.1 `+one+`&nbsp;&nbsp;No.2 `+two+`&nbsp;&nbsp;No.3 `+three+`</h3>
             <h6>本日最低累计充值大于`+minmoney+`(第一千名充值金额)有概率入榜</h6><br>
-            <h6>本页面将于活动结束后下线</h6><br>
+            <h6>本页面将于活动结束后下线,温馨提示:需要入榜拿奖励的,需要保持每日充值至活动结束且一直在榜</h6><br>
+            <h6>合理游戏,理性消费</h6><br>
             <h7>本页面为排行榜数据仅统计上榜数据,未上榜富哥无法统计</h7>
           </body>
         </html>
